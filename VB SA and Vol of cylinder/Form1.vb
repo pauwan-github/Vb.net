978aacb6155254cdbef866defb025e1e0e9a68ae
pauwan-github <132430823+pauwan-github@users.noreply.github.com>
2024-02-12 16:31:27 +0300

    Add files via upload
    This is a vb .net code for finding the surface area and volume of a cylinder.

---
+++ VB SA and Vol of cylinder/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Const Pi As Single = 3.142
+            Dim radius, height, surfaceArea, volume As Single
+            radius = Val(TextBox1.Text)
+            height = Val(TextBox2.Text)
+            surfaceArea = (2 * Pi * radius * radius) + (Pi * (2 * radius) * height)
+            volume = Pi * radius * radius * height
+            TextBox3.Text = surfaceArea
+            TextBox4.Text = volume
+        End Sub
+End Class
